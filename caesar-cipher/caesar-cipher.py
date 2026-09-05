5f0c646e8f26e115c41068f50fc71e764d8c3a37
Hafize Nur Genek <genekhafizenur12@gmail.com>
2026-09-05 15:44:02 +0300

    feat: improve caesar cipher menu with input validation

--- caesar-cipher/caesar-cipher.py
+++ caesar-cipher/caesar-cipher.py
@@ -1,19 +1,71 @@
-metin = input("Metin: ")
-kaydirma = int(input("Kaydırma değeri: "))
-
 def sifreleme(metin, kaydirma):
     yeni_metin = ""
     for harf in metin:
-        taban = ord("a")
-        pozisyon = ord(harf) - taban
-        yeni_pozisyon = (pozisyon + kaydirma) % 26 #Wrap-around: alfabenin sonuna gelen bir harfi kaydırırken, % 26 ile pozisyonu 0-25 aralığında tutup alfabenin başına geri sardırma işlemi.
-        yeni_harf = chr(yeni_pozisyon + taban)
-        yeni_metin += yeni_harf
-    print(yeni_metin)
-    return yeni_metin
+        if harf.isalpha():
+            if harf.islower():
+                taban = ord("a")
+                pozisyon = ord(harf) - taban
+                yeni_pozisyon = (pozisyon + kaydirma) % 26 #Wrap-around: alfabenin sonuna gelen bir harfi kaydırırken, % 26 ile pozisyonu 0-25 aralığında tutup alfabenin başına geri sardırma işlemi.
+                yeni_harf = chr(yeni_pozisyon + taban)
+                yeni_metin += yeni_harf
+            else:
+                taban = ord("A")
+                pozisyon = ord(harf) - taban
+                yeni_pozisyon = (pozisyon + kaydirma) % 26 #Wrap-around: alfabenin sonuna gelen bir harfi kaydırırken, % 26 ile pozisyonu 0-25 aralığında tutup alfabenin başına geri sardırma işlemi.
+                yeni_harf = chr(yeni_pozisyon + taban)
+                yeni_metin += yeni_harf
+        else:
+            yeni_metin += harf
+    print(f"\nSonuç: {yeni_metin}")
 
-def sifre_coz():
-    pass
+def sifre_coz(metin,kaydirma):
+    cozulmus_metin = ""
+    for harf in metin:
+        if harf.isalpha():
+            if harf.islower():
+                taban = ord("a")
+                pozisyon = ord(harf) - taban
+                yeni_pozisyon = (pozisyon - kaydirma) % 26 
+                yeni_harf = chr(yeni_pozisyon + taban)
+                cozulmus_metin += yeni_harf
+            else:
+                taban = ord("A")
+                pozisyon = ord(harf) - taban
+                yeni_pozisyon = (pozisyon - kaydirma) % 26 
+                yeni_harf = chr(yeni_pozisyon + taban)
+                cozulmus_metin += yeni_harf
+        else:
+            cozulmus_metin += harf
+    print(f"\nSonuç: {cozulmus_metin}")
 
+while True:
+    print("\n" + "="*30)
+    print("   CAESAR CIPHER")
+    print("="*30)
+    print("1. Şifre Oluşturmak")
+    print("2. Şifre Çözmek")
+    print("3. Çıkış yap")
+    print("="*30)
 
-sifreleme(metin,kaydirma)
+    secim = input("\nSeciminizi girin: (1,2,3) ")
+
+    if secim == "1":
+        metin = input("\nMetin: ")
+        kaydirma = int(input("Kaydırma değeri: "))
+        if kaydirma <= 0:
+            print("Kaydırma değeri en az 1 olmalı!")
+            continue
+        sifreleme(metin,kaydirma)
+    elif secim == "2":
+        metin = input("Metin: ")
+        kaydirma = int(input("Kaydırma değeri: "))
+        if kaydirma <= 0:
+            print("Kaydırma değeri en az 1 olmalı!")
+            continue
+        sifre_coz(metin,kaydirma)
+    elif secim == "3":
+        print("Çıkış yapıyorsunuz.")
+        break
+    else:
+        print("Geçersiz seçim, lütfen 1, 2 veya 3 girin.")
+        continue
